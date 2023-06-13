dff923167e672d5f8c2c73af41b610fc7b82c6ea
Zakaria Abdul-Rasheed <zakariaabdulrasheed2@gmail.com>
2023-06-13 07:08:48 +0000

    frontend templates

---
+++ Ecom/func.js
@@ -0,0 +1,91 @@
+let menu = document.querySelector("#menu-bar");
+let navbar = document.querySelector(".navbar");
+let header = document.querySelector(".header-2");
+
+menu.addEventListener("click", () => {
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+});
+
+window.onscroll = () => {
+  menu.classList.remove("fa-times");
+  navbar.classList.remove("active");
+
+  if (window.scrollY > 150) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+};
+
+/* home swipper section starts */
+
+var swiper = new Swiper(".home-slider", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop: true,
+});
+
+var swiper = new Swiper(".review-slider", {
+  spaceBetween: 20,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
+  },
+});
+
+/* home swipper section ends */
+
+
+/**Timer section starts */
+let countDate = new Date('june 10, 2023 00:00:00').getTime();
+
+function CountDown(){
+  let now = new Date().getTime();
+  gap = countDate - now;
+
+  let second = 1000;
+  let minute = second * 60;
+  let hour = minute * 60;
+  let day = hour * 24;
+
+  let d = Math.floor(gap/(day));
+  let h = Math.floor((gap % (day)) / (hour));
+  let m = Math.floor((gap % (hour)) / (minute));
+  let s = Math.floor((gap % (minute)) / (second));
+
+
+  document.getElementById('day').innerText = d;
+  document.getElementById('hour').innerText = h;
+  document.getElementById('minute').innerText = m;
+  document.getElementById('second').innerText = s;
+}
+
+setInterval(function(){
+  CountDown();
+}, 1000)
+/** Timer section ends*/
